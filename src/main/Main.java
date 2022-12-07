8c47db4fe710753da85a9577032bb4f33f94b9a9
Braian Granero <102628539+BraG29@users.noreply.github.com>
2022-12-07 16:09:39 -0300

    Update Main.java
    update

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		//Parte 1
-		//System.out.println(suma(1, 2, 3));
+		System.out.println(suma(1, 2, 3));
 
 		//Parte 2
 		Coche c1 = new Coche(2);
